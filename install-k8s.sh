ff6b7b4d56e5578916d59298ee8ecce8d6655ebe
anouarharrou <anouar_-_harrou@outlook.fr>
2024-01-28 22:42:15 +0100

    new update

--- install-k8s.sh
+++ install-k8s.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
-# copy this script and run in all master and worker nodes
-#) pre-requisites
-#1) Ubuntu 20.04 LTS
-#2) Internet connection
-#3) Disable swap
 
 # Function to check if a Kubernetes cluster already exists
 check_k8s_cluster() {
@@ -24,14 +19,14 @@ check_docker_containerd() {
 # Verify if a Kubernetes cluster exists
 check_k8s_cluster
 
-#i1) Switch to root user [ sudo su], if you don't have root user access use sudo before each command.
+# Switch to root user
+sudo su
 
-#2) Disable swap & add kernel settings
-
+# Disable swap & add kernel settings
 swapoff -a
 sed -i '/ swap / s/^\(.*\)$/#\1/g' /etc/fstab
 
-#optional: Set up hostnames -you can give unique hostnames for nodes so that Kubernetes can use these names to identify nodes
+# Set up hostnames
 hostnamectl set-hostname "master-node"
 exec bash
 
@@ -49,8 +44,7 @@ else
   echo "Entries added to /etc/hosts."
 fi
 
-#3) Add  kernel settings & Enable IP tables(CNI Prerequisites)
-
+# Add kernel settings & Enable IP tables(CNI Prerequisites)
 cat <<EOF | sudo tee /etc/modules-load.d/k8s.conf
 overlay
 br_netfilter
@@ -67,25 +61,20 @@ EOF
 
 sysctl --system
 
-#4) Install containerd run time
-
-#To install containerd, first install its dependencies.
-
+# Install containerd runtime
 apt-get update -y
 apt-get install ca-certificates curl gnupg lsb-release -y
 
-#Note: We are not installing Docker Here.Since containerd.io package is part of docker apt repositories hence we added docker repository & it's key to download and install containerd.
-# Add Docker’s official GPG key:
+# Add Docker’s official GPG key
 sudo mkdir -p /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 
-#Use follwing command to set up the repository:
-
+# Set up the repository
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
-# check if containerd runtime exists
+# Check if containerd runtime exists
 check_docker_containerd
 
 # Install containerd
@@ -93,52 +82,33 @@ apt-get update -y
 apt-get install containerd.io -y
 
 # Generate default configuration file for containerd
-
-#Note: Containerd uses a configuration file located in /etc/containerd/config.toml for specifying daemon level options.
-#The default configuration can be generated via below command.
-
 containerd config default > /etc/containerd/config.toml
 
-# Run following command to update configure cgroup as systemd for contianerd.
-
+# Update configure cgroup as systemd for containerd
 sed -i 's/SystemdCgroup \= false/SystemdCgroup \= true/g' /etc/containerd/config.toml
 
 # Restart and enable containerd service
-
 systemctl restart containerd
 systemctl enable containerd
 
-#5) Installing kubeadm, kubelet and kubectl
-
-# Update the apt package index and install packages needed to use the Kubernetes apt repository:
-
+# Install kubeadm, kubelet, and kubectl
 apt-get update
 apt-get install -y apt-transport-https ca-certificates curl
 
-# Download the Google Cloud public signing key:
-
 curl -fsSL https://dl.k8s.io/apt/doc/apt-key.gpg | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-archive-keyring.gpg
 
-# Add the Kubernetes apt repository:
-
 echo "deb [signed-by=/etc/apt/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
-
-# Update apt package index, install kubelet, kubeadm and kubectl, and pin their version:
 
 apt-get update
 apt-get install -y kubelet kubeadm kubectl
 
-# apt-mark hold will prevent the package from being automatically upgraded or removed.
-
 apt-mark hold kubelet kubeadm kubectl
-
-# Enable and start kubelet service
 
 systemctl daemon-reload
 systemctl start kubelet
 systemctl enable kubelet.service
 
-#Initialize the Kubernetes cluster on the master node
+# Initialize the Kubernetes cluster on the master node
 kubeadm config images pull
 kubeadm init --pod-network-cidr=10.10.0.0/16
 
@@ -146,23 +116,11 @@ mkdir -p $HOME/.kube
 sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
 
-#Deploy Calico network
+# Deploy Calico network
 kubectl create -f https://raw.githubusercontent.com/projectcalico/calico/v3.26.1/manifests/tigera-operator.yaml
 curl https://raw.githubusercontent.com/projectcalico/calico/v3.26.1/manifests/custom-resources.yaml -O
 sed -i 's/cidr: 192\.168\.0\.0\/16/cidr: 10.10.0.0\/16/g' custom-resources.yaml
 kubectl create -f custom-resources.yaml
 
-#Add worker nodes to the cluster: this command is provided at the end of kubeadm init command output
-kubeadm join &lt;MASTER_NODE_IP>:&lt;API_SERVER_PORT> --token &lt;TOKEN> --discovery-token-ca-cert-hash &lt;CERTIFICATE_HASH>
-
-#Verify the cluster status
-kubectl get nodes
-
-#list all pods in all namespaces
-kubectl get pods --all-namespaces
-
-#confirm that all system pods are running
-kubectl get pods --all-namespaces -o wide
-
-#congratulations! you have successfully created a Kubernetes cluster with one master node and one worker node.
 echo "congratulations! you have successfully created a Kubernetes cluster with one master node and one worker node."
+echo "You can now join any number of worker nodes by running the following on each as root:"
